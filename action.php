0619c01103330a3bb730cff705721a0a4edf66d0
amrit-kandel-web <98876854+amrit-kandel-web@users.noreply.github.com>
2023-02-07 10:37:58 +0200

    add some line
    adding some line

--- action.php
+++ action.php
@@ -1,20 +1,10 @@
 <?PHP include "header.php";
-$fname = $_POST[ 'fname'];
-$lname = $_POST[ 'lname'];
-$dob = $_POST[ 'bdate'];
-$color = $_POST[ 'color'];
+$fname = $_POST['fname'];
+$lname = $_POST['lname'];
+$dob = $_POST['bdate'];
+$color = $_POST['color'];
 echo "<h3> Your name is $fname $lname /. your favourite color is $color and your date of birth is $dob </h3>";
-
-
-
-
-
-
-
-
-
-
 
 ?>
 
-<?PHP include "footer.php"?>
+<?PHP include "footer.php" ?>
--- variable.php
+++ variable.php
@@ -3,13 +3,18 @@ $title= "Exercise 3";
 <h3> Exercise 3. In-class Task variable & Operators (variable.php)</h3>
 <h2>
     Create a simple html form to get Firstname and Lastname from the user and use echo echo statement to print using
-    tag: Hello …., You are welcome to my site. 
-    </h2>
-        <form action="action.php" method="post">
-            First Name: <input type="text" name="fname"> <br>
-            Last Name: <input type="text" name="lname"> <br>
-            Date: <input type="date" name="bdate"> <br>
-            select fav Color: <input type="color" name="color"> <br>
-             <input type="submit" value="submit">
+    tag: Hello …., You are welcome to my site.
+</h2>
+<form action="action.php" method="post">
+    <div class="row">
+        <div class="col">
+            <input type="text" name="fname" required placeholder="First Name" class="form-control">
+        </div>
+        <div class="col">
+            <input type="text" name="lname" required placeholder="Last Name" class="form-control">
+        </div>
+        Birth Date: <input type="date" name="bdate"> <br>
+        select fav Color: <input type="color" name="color"> <br>
+        <input type="submit" value="submit">
 </form>
-<?PHP include "footer.php"?>
+<?PHP include "footer.php" ?>
